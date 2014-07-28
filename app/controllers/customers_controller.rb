17bfa6b9684d3d404b473a0d089ed53873fe2436
JonnyHarris <jonnybg@live.com>
2014-07-28 19:56:34 +0100

    Add Phone numbr to customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -69,6 +69,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.require(:customer).permit(:firstname, :lastname, :address)
+      params.require(:customer).permit(:firstname, :lastname, :address, :phone)
     end
 end
--- app/views/customers/index.json.jbuilder
+++ app/views/customers/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@customers) do |customer|
-  json.extract! customer, :id, :firstname, :lastname, :address
+  json.extract! customer, :id, :firstname, :lastname, :address, :phone
   json.url customer_url(customer, format: :json)
 end
